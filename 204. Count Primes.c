81da59a3fc0d8de6a4144551c5fae21866ee80dc
feixingjishu <37857542+feixingjishu@users.noreply.github.com>
2018-09-14 15:05:32 +0800

    Sieve of Eratosthenes(埃拉托色尼筛选法)

---
+++ 204. Count Primes.c
@@ -0,0 +1,35 @@
+/**
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+/*
+
+int countPrimes(int n) {
+    int i, j;
+    int cnt = 0;
+    bool* cont = (bool* )malloc(sizeof(bool)*n);
+    memset(cont, true, sizeof(bool)*n);
+    for(i=2; i*i<n; i++)
+    {
+        if(cont[i] == false)
+        {
+            continue;
+        }
+        for(j=i*i; j<n ;j+=i)
+        {
+            cont[j] = false;
+        }
+    }
+    for(i=2; i<n; i++)
+    {
+        if(cont[i] == true)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
